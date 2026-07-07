0351ddf0c3d89061817821820c443d6c905a24bd
sanaln27 <sanalnadaf786@gmail.com>
2026-07-07 16:49:25 +0530

    New seperate models folder,ik I would have done it earlier but still heheheh

--- model/config.py
+++ model/config.py
@@ -6,10 +6,16 @@ load_dotenv()
 
 
 
-model=ChatOpenAI(
+llm=ChatOpenAI(
     model="openai/gpt-5-nano",
     base_url="https://api.aicredits.in/v1",
     api_key=os.getenv("OPENAI_API_KEY"),
     timeout=60
 )
 
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small",
+    base_url="https://api.aicredits.in/v1",
+    api_key=os.getenv("OPENAI_API_KEY"),
+    timeout=60,
+)

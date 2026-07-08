f36c4c1b546e7664efaa6003ee2241c484691207
sanaln27 <sanalnadaf786@gmail.com>
2026-07-08 18:24:45 +0530

    Tme bio exam stilll Cant figure out what is the problemm ,uk what I AM DUMBBBBBBB

--- RAG/chunking.py
+++ RAG/chunking.py
@@ -6,13 +6,21 @@ from langchain_community.document_loaders import (
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+
 from model.config import embeddings
 load_dotenv()
 
 print(os.getenv("OPENAI_API_KEY")[:10])
 
 
-embeddings=embeddings
+embeddings=embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small",
+    base_url="https://api.aicredits.in/v1",
+    api_key=os.getenv("OPENAI_API_KEY"),
+    timeout=60,
+)
+
 
 def build_vector_database():
     documents = []

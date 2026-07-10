5950692772542bc3205db714a7f721b1b9ee1d63
sanaln27 <sanalnadaf786@gmail.com>
2026-07-10 05:59:42 +0530

    Bhai abhe retrive ek dam badhiyaa kar raha hai

--- RAG/retriver.py
+++ RAG/retriver.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from  langchain_chroma import Chroma
 from model.config import embeddings
-from .ingest import get_vector
+from .store_db import get_vector
 vectorstore=get_vector()
 
 retriver=vectorstore.as_retriever(search_kwags={"k":3})

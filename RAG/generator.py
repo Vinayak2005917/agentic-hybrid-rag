7ce2fddcf4ab673d924303c8028f82338b59f632
sanaln27 <sanalnadaf786@gmail.com>
2026-07-11 23:19:33 +0530

    so sleepy man

--- RAG/generator.py
+++ RAG/generator.py
@@ -1,1 +1,13 @@
 from model.config import llm
+from .retriver import retrive_documents
+from prompts.rag_prompt import rag_prompt
+
+def generate_answer(question:str):
+    documents=retrive_documents(question)
+    content="\n\n".join(
+        doc.page_content for doc in documents
+    )
+    prompt=rag_prompt(
+        "context"
+    )
+
